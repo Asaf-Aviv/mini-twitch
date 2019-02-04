c929f09e97a6b88fc9676d89483464e14a8e7a19
Asaf Aviv <asafaviv89@gmail.com>
2019-02-04 12:12:10 +0200

    build

---
+++ client/build/precache-manifest.ef4c4e4283c1060230e1c2e21a15f3cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac6d2da9aa251f8bda54fbad08f61fe0",
+    "url": "/static/media/hand.ac6d2da9.svg"
+  },
+  {
+    "revision": "65372bb9e61bb590e5297f9f4c7cc524",
+    "url": "/static/media/close.65372bb9.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12353f259f942df6ba89",
+    "url": "/static/js/main.12353f25.chunk.js"
+  },
+  {
+    "revision": "0df453d2e6b05d4fb090",
+    "url": "/static/js/1.0df453d2.chunk.js"
+  },
+  {
+    "revision": "12353f259f942df6ba89",
+    "url": "/static/css/main.45a541a0.chunk.css"
+  },
+  {
+    "revision": "5772fd82488ca48229a6da8df6ee7cb1",
+    "url": "/index.html"
+  }
+];
