97ac65493aea17200ed93516f0f4e0088f82fa0b
Asaf Aviv <asafaviv89@gmail.com>
2019-02-04 12:36:48 +0200

    serve static files

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
